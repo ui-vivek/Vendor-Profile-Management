412cdf39c162008420785023a82802d5cef572d3
ui-vivek <cu.vivek.1557@gmail.com>
2024-08-22 12:12:48 +0530

    db url

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
 @Module({
-  imports: [MongooseModule.forRoot(process.env.mongoDb_url)],
+  imports: [ MongooseModule.forRoot(process.env.MONGO_DB_URL || 'mongodb://localhost:27017/mydatabase'),],
   exports: [MongooseModule], 
     })
 
